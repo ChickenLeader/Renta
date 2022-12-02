50a825aa73ae6b2eacbac3c95c9329d7ac9f9bc4
mahmoud.s.m619@gmail.com <mahmoud.s.m619@gmail.com>
2022-12-02 23:42:41 +0200

    propertyCard adjustments

--- components/HomeComponents/propertyCard/propertyCard.js
+++ components/HomeComponents/propertyCard/propertyCard.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from "./property.module.css";
 import Image from "next/image";
 import Text from "components/General/Text";
 import { Colors } from "constants/Colors";
-import { Card } from "antd";
+import { Card, Carousel } from "antd";
 import { FontFamily } from "constants/FontFamily";
+import { FiArrowRight, FiArrowLeft } from "react-icons/fi";
+
 const img = require("../../../public/assets/houseCardx2.png");
 
 const PropertyCard = () => {
   const [loading, setLoading] = useState(false);
+  const sliderRef = useRef();
   let tempIcons = [
     {
       id: 1,
@@ -51,12 +54,29 @@ const PropertyCard = () => {
   return (
     <Card hoverable className={styles.cardCon}>
       <Row>
-        <Col lg={3} md={4} xs={4} className="p-0">
-          <div className={styles.cardImgCon}>
-            <Image src={img} alt="property card" layout="fill" />
+        <Col md={4} className="p-0 position-relative">
+          <div className={styles.directions}>
+            <div className={styles.prev} onClick={() => sliderRef.current.prev()}>
+              <FiArrowLeft size={22} color="white" />
+            </div>
+            <div className={styles.next} onClick={() => sliderRef.current.next()}>
+              <FiArrowRight size={22} />
+            </div>
           </div>
+          <Carousel ref={sliderRef} dots={{ className: styles.test }}>
+            {[1, 2, 3, 4].map((item) => (
+              <div key={item + ""} className={styles.cardImgCon}>
+                <Image
+                  src={img}
+                  alt="property card"
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+            ))}
+          </Carousel>
         </Col>
-        <Col lg={5} md={4} xs={4} className="p-0">
+        <Col md={4} xs={12} className="p-0">
           <div className={styles.details}>
             <div>
               <Text fontSize={20} className="mb-3">
@@ -84,15 +104,15 @@ const PropertyCard = () => {
             </div>
           </div>
         </Col>
-        <Col lg={4} md={4} xs={4} className={styles.iconsSection}>
+        <Col md={4} xs={12} className={styles.iconsSection}>
           <div className={styles.iconsCon}>
-            <Row className="h-100 align-items-center d-flex justify-content-center border-start border-2 m-auto">
+            <Row className="h-100 align-items-center d-flex justify-content-center m-auto">
               {tempIcons.map((item) => {
                 return (
                   <Col
                     lg={5}
                     md={6}
-                    xs={12}
+                    xs={6}
                     className="d-flex align-items-center justify-content-center p-0"
                   >
                     <Image
--- pages/Latest/index.js
+++ pages/Latest/index.js
@@ -1,12 +1,12 @@
-import ScreenWrapper from "components/General/ScreenWrapper";
-import PropertyCard from "components/HomeComponents/propertyCard/propertyCard";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-
+import ScreenWrapper from "components/General/ScreenWrapper";
+import PropertyCard from "components/HomeComponents/propertyCard/propertyCard";
+import styles from "./latest.module.css"
 const LatestSeen = () => {
   return (
     <ScreenWrapper>
-      <div className="my-3 mx-5">
+      <div className={styles.latestContainer}>
         <Row className="justify-content-around">
           {[1, 2, 3, 4, 5, 6, 7, 8].map(() => (
             <Col lg={6}>
