79c58dea9edf121e312f9b486088fc6506ac3ebe
mahmoud.s.m619@gmail.com <mahmoud.s.m619@gmail.com>
2023-02-19 21:17:32 +0200

    my units re-structure

--- components/HomeComponents/propertyCard/unitCard.js
+++ components/HomeComponents/propertyCard/unitCard.js
@@ -6,11 +6,12 @@ import Text from "components/General/Text";
 import { Colors } from "constants/Colors";
 import { Card } from "antd";
 import { useRouter } from "next/router";
+import { FontFamily } from "constants/FontFamily";
 const img = require("../../../public/assets/houseCardx2.png");
 
-const UnitCard = () => {
+const UnitCard = ({ item }) => {
   const [loading, setLoading] = useState(false);
-  const navigate = useRouter()
+  const navigate = useRouter();
   let tempIcons = [
     {
       id: 1,
@@ -49,68 +50,94 @@ const UnitCard = () => {
       digit: 167,
     },
   ];
+
+  const unitDetails = [
+    { id: 1, title: "Renter name:", value: item.name },
+    { id: 2, title: "Rent amount per month:", value: item.rent },
+    { id: 3, title: "Contract start:", value: item.startDate },
+    { id: 4, title: "Contract end:", value: item.endDate },
+    { id: 5, title: "Contract photo:", value: item.photo, underline: true },
+    { id: 6, title: "Renter id photo:", value: item.idPhoto, underline: true },
+  ];
   return (
     <Card
       hoverable
       className={styles.unitCardCon}
       onClick={() => navigate.push("/detail/jghghbbj")}
     >
-      <Row>
-        <Col md={4} className="p-0 position-relative">
-          <div className={styles.cardImgCon}>
-            <Image
-              src={img}
-              alt="property card"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </Col>
-        <Col
-          md={4}
-          className="p-0 flex-column align-items-center justify-content-center"
+      <div className={styles.cardHeader}>
+        <Row
+          className={`justify-content-around align-items-center ${styles.cardDetails}`}
+          style={{ backgroundColor: "#f4f4f4" }}
         >
-          <div className={styles.details}>
-            <div>
-              <Text fontSize={20} className={styles.propertyType}>
-                Small apartment
-              </Text>
-              <Text color={Colors.primary} fontSize={14}>
-                Mountain View, New cairo
-              </Text>
+          <Col
+            md={5}
+            className=" flex-column align-items-center justify-content-center"
+          >
+            <div className={styles.details}>
+              <div>
+                <Text fontSize={20} className={styles.propertyType}>
+                  Small apartment
+                </Text>
+                <Text color={Colors.primary} fontSize={14}>
+                  Mountain View, New cairo
+                </Text>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="ps-0">
-          <div className={styles.iconsCon}>
-            <Row
-              style={{ minHeight: 120 }}
-              className="align-items-center p-1 d-flex justify-content-center border-start border-1 m-auto"
-            >
-              {tempIcons.map((item) => {
-                return (
-                  <Col
-                    key={item.id + ""}
-                    lg={4}
-                    md={6}
-                    xs={6}
-                    className="d-flex align-items-center justify-content-center p-0"
-                  >
-                    <Image
-                      src={item.icon}
-                      alt="space icon"
-                      objectFit="contain"
-                      width={25}
-                      height={25}
-                    />
-                    <Text className="d-inline ms-1">{item.digit}</Text>
-                  </Col>
-                );
-              })}
-            </Row>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={6} className="ps-0">
+            <div className={styles.iconsCon}>
+              <Row
+                style={{ minHeight: 120 }}
+                className="align-items-center p-1 d-flex justify-content-center border-start border-1 m-auto"
+              >
+                {tempIcons.map((item) => {
+                  return (
+                    <Col
+                      key={item.id + ""}
+                      lg={4}
+                      md={6}
+                      xs={6}
+                      className="d-flex align-items-center justify-content-center p-0"
+                    >
+                      <Image
+                        src={item.icon}
+                        alt="space icon"
+                        objectFit="contain"
+                        width={25}
+                        height={25}
+                      />
+                      <Text className="d-inline ms-1">{item.digit}</Text>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </div>
+
+      <div>
+        <div className="ms-3">
+          {unitDetails.map((detail) => {
+            return (
+              <div className={"d-flex align-items-center my-5"}>
+                <div style={{ width: 8, height: 8, background: "#D6D7DB" }} />
+                <Text className={"mx-2"}>{detail.title}</Text>
+                <Text
+                  fontSize={18}
+                  fontFamily={FontFamily.medium}
+                  style={{
+                    textDecoration: detail.underline ? "underline" : "none",
+                  }}
+                >
+                  {detail.value}
+                </Text>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Card>
   );
 };
--- pages/Units/index.js
+++ pages/Units/index.js
@@ -7,6 +7,9 @@ import { FontFamily } from "constants/FontFamily";
 import { Col, Row } from "react-bootstrap";
 import styles from "./units.module.css";
 import UnitCard from "components/HomeComponents/propertyCard/unitCard";
+import { Colors } from "constants/Colors";
+import { logoutHandler } from "hooks/logoutHandler";
+import Image from "next/image";
 
 const Units = () => {
   let myUnits = [
@@ -34,60 +37,37 @@ const Units = () => {
     <ScreenWrapper>
       <Container>
         <div className="my-2">
-          <Text
-            fontSize={64}
-            color="#162137"
-            fontFamily={FontFamily.bold}
-            className="my-5"
+          <div
+            className="d-flex justify-content-between align-items-center my-5 mx-2"
+            style={{ width: "80%" }}
           >
-            my units
-          </Text>
-          <Row className="justify-content-between">
+            <Text fontSize={64} color="#162137" fontFamily={FontFamily.bold}>
+              my units
+            </Text>
+            <div className="pointer" onClick={logoutHandler}>
+              <Text
+                fontSize={18}
+                color={Colors.primary}
+                fontFamily={FontFamily.bold}
+                style={{ textDecoration: "underline" }}
+              >
+                Logout
+              </Text>
+            </div>
+          </div>
+          <Row className="justify-content-between align-items-center">
             {myUnits.map((item) => {
               return (
-                <Col key={item.id + ""} lg={6}>
-                  <UnitCard />
-                  <div className="ms-3">
-                    <div className={styles.unitDetails}>
-                      <Text>Renter name</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.name}
-                      </Text>
-                    </div>
-                    <div className={styles.unitDetails}>
-                      <Text>Rent amount per month</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.rent}
-                      </Text>
-                    </div>
-                    <div className={styles.unitDetails}>
-                      <Text>Contract start</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.startDate}
-                      </Text>
-                    </div>
-                    <div className={styles.unitDetails}>
-                      <Text>Contract end</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.endDate}
-                      </Text>
-                    </div>
-                    <div className={styles.unitDetails}>
-                      <Text>Contract photo</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.photo}
-                      </Text>
-                    </div>
-                    <div className={styles.unitDetails}>
-                      <Text>Renter id photo</Text>
-                      <Text fontSize={18} fontFamily={FontFamily.medium}>
-                        {item.idPhoto}
-                      </Text>
-                    </div>
-                  </div>
+                <Col key={item.id + ""} lg={5}>
+                  <UnitCard item={item} />
                 </Col>
               );
             })}
+            <Col lg={2}>
+              <div className={styles.arrowCon}>
+                <Image src={require("public/assets/UnitsArrow.svg")} />
+              </div>
+            </Col>
           </Row>
         </div>
       </Container>
